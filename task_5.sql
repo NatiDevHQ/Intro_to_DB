09865aea433627655f0e72ccfa2deddb2d9c1484
NATNAEL GETACHEW <natigech35@gmail.com>
2025-06-29 20:38:06 +0000

    task 5  Insert a single row into the CUSTOMERS table

---
+++ task_5.sql
@@ -0,0 +1,3 @@
+-- Insert a single row into the CUSTOMERS table
+INSERT INTO CUSTOMERS (CUSTOMER_ID, FIRST_NAME, LAST_NAME, EMAIL, PHONE)
+VALUES (1, 'Cole', 'Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
